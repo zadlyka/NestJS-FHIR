b9cd454808680ca765a54a345c753fd12fd69f1e
zadlyka <tara02zadlyka@gmail.com>
2022-08-11 11:56:26 +0700

    change readme and env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatientModule } from './patient/patient.module';
+require('dotenv').config()
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/testing-fhir'), PatientModule],
+  imports: [MongooseModule.forRoot(`mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DBNAME}`), PatientModule],
   controllers: [AppController],
   providers: [AppService],
 })
